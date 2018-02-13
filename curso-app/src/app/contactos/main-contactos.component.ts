97c85ac945aa3538f0ab3aec3da1fa767d779ca5
alce65 <alce65@hotmail.es>
2018-02-13 23:45:20 +0100

    Http Rx y json-API  agenda

--- curso-app/src/app/contactos/main-contactos.component.ts
+++ curso-app/src/app/contactos/main-contactos.component.ts
@@ -16,28 +16,42 @@ export class MainContactosComponent implements OnInit {
 
   ngOnInit() {
     this.aContactos = [];
-    this.contactosService.getContactos().then(
+    /* this.contactosService.getContactos().then(
        response =>  this.aContactos = response
-     );
+     ); */
+     this.contactosService.getContactos$().subscribe(
+      response =>  this.aContactos = response
+    );
   }
 
   // respuesta a los eventos en el componente lista
   deleteContacto (i) {
-    this.contactosService.deleteContacto(i)
+    /* this.contactosService.deleteContacto(i)
     .then(
       () => {this.contactosService.getContactos()
         .then(response =>  this.aContactos = response);
-      });
+      }); */
+      this.contactosService.deleteContacto$(i)
+      .subscribe(
+        () => {this.contactosService.getContactos$()
+          .subscribe(response =>  this.aContactos = response);
+        });
+
 
     // TODO hacer que lista se actualice con los nuevos datos
   }
 
   // respuesta a los eventos en el componente altas
   addContacto (oContacto) {
-    this.contactosService.setContactos(oContacto)
+    /* this.contactosService.setContactos(oContacto)
     .then(
       () => {this.contactosService.getContactos()
         .then(response =>  this.aContactos = response);
+      }); */
+    this.contactosService.setContactos$(oContacto)
+    .subscribe(
+      () => {this.contactosService.getContactos$()
+        .subscribe(response =>  this.aContactos = response);
       });
   }
 }
--- curso-app/src/app/services/contactos.service.ts
+++ curso-app/src/app/services/contactos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Contacto } from '../models/contactos.model';
 import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 
 // El servicio contactos se ocupa de almacenar la lista de contactos
@@ -9,6 +10,7 @@ const URLAPI = 'http://localhost:3000/contactos';
 
 @Injectable()
 export class ContactosService {
+
     private sURL: string;
     private aContactos: Array<Contacto>;
 
@@ -26,15 +28,32 @@ export class ContactosService {
         });
     }
 
+    getContactos$(): Observable<Array<Contacto>> {
+        // const aLibros: Array<string>  = [];
+        // return this.http.get(this.sURL);
+        return this.http.get<Contacto[]>(this.sURL);
+    }
+
     setContactos(oContacto) {
         console.log('Enviando datos');
         console.log(oContacto);
         return this.http.post(this.sURL, oContacto).toPromise();
     }
 
+    setContactos$(oContacto): Observable<any> {
+        console.log('Enviando datos');
+        console.log(oContacto);
+        return this.http.post(this.sURL, oContacto);
+    }
     deleteContacto (i) {
         i++;
         return this.http.delete(this.sURL + '/' + i).toPromise();
+        // this.aContactos.splice(i, 1);
+      }
+
+      deleteContacto$(i): Observable<any> {
+        i++;
+        return this.http.delete(this.sURL + '/' + i);
         // this.aContactos.splice(i, 1);
       }
 }
